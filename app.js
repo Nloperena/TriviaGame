efd56a4f9597a2faf698d0e2c562664d2aa7e6c3
Nicholas Loperena <nicholasloperena@gmail.com>
2019-06-07 23:01:07 -0400

    added a bunch of code but the only thing I could working is the timer. Having a hard time figuring out how to transition this into a new questions

--- app.js
+++ app.js
@@ -2,85 +2,136 @@ var running = false;
 
 // declaring variables
 var intervalID
+var next = 0
 var j = 0
 var correct = 0
 var incorrect = 0
+
+
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const choiceC = document.getElementById("C")
 // Step1
 var card = $("#quiz-area");
 
 // Step2
+//starting timer
 var count = 10;
 
-// Step3
-
-var correctAnswers = ["1", "2", "3", "4", "5"];
 
 
+
+//questions
 var myQuestions = [{
-        question: "This is my first question ",
-        answers: ["A1 ", "A2 ", "A3 ", "A4 "],
+        question: "What color is an Apple? ",
+        choiceA: "Red ",
+        choiceB: "Blue ",
+        choiceC: "Purple ",
+        choiceD: "Black ",
+        correct: "A"
         // gif: //link to image
     },
     {
-        question: "This is my second question ",
-        answers: ["B1 ", "B2 ", "B3 ", "B4 "],
+        question: "What color is an Grape? ",
+        choiceA: "Red ",
+        choiceB: "Blue ",
+        choiceC: "Purple ",
+        choiceD: "Black ",
+        correct: "C"
         // gif: //link to image
     },
     {
-        question: "This is my third question ",
-        answers: ["C1 ", "C2 ", "C3  ", "C4 "],
+        question: "What color is the sky? ",
+        choiceA: "Red ",
+        choiceB: "Blue ",
+        choiceC: "Purple ",
+        choiceD: "Black ",
+        correct: "B"
         // gif: //link to image;
     },
     {
-        question: "This is my fourth question ",
-        answers: ["D1 ", "D2 ", "D3 ", "D4 "],
+        question: "What color is represents royalty? ",
+        choiceA: "Red ",
+        choiceB: "Blue ",
+        choiceC: "Purple ",
+        choiceD: "Black ",
+        correct: "C"
         // gif: //link to image
     },
     {
-        question: "This is my fifth question ",
-        answers: ["E1 ", "E2 ", "E3 ", "E4 "],
+        question: "What color is fire on Earth usually? ",
+        choiceA: "Red ",
+        choiceB: "Blue ",
+        choiceC: "Purple ",
+        choiceD: "Black ",
+        correct: "A"
         // gif: //link to image
     }
 ]
 
+//created this variable to try and work with my other function on line 88
+var questionIndex = myQuestions.length- 1;
 
-var game = {
-    //Step4
-    name: function () {
-        i = 0;
-        console.log(myQuestions[i].question)
-        for (i = 0; i < 4; i++) {
-            console.log(myQuestions[j].answers[i])
-        }
-        j++
-    }
-}
-game.name()
 
-// Step5
+//created an object called game that holds some of the logic for the game.
+// var game = {
+//     name: function () {
+//         i = 0;
+//         console.log(myQuestions[i].question)
+//         for (i = 0; i < 4; i++) {
+//             console.log(myQuestions[j].answers[i])
+//         }
+//         j++
+//     }
+// }
+// game.name()
+
+//This function will allow the next question to swap to the following
+// function showQuestion(){
+//     let quest = myQuestions[next];
+//     question.innerHTML ="<p>" +quest.myQuestions+ "</p>";
+//     choiceA.innerHTML = quest.ChoiceA
+//     choiceB.innerHTML = quest.ChoiceB
+//     choiceC.innerHTML = quest.ChoiceC
+//     choiceD.innerHTML = quest.ChoiceD
+// }
+// next = 0;
+// showQuestion()
+// next++
+// showQuestion()
+
+
+// This is the timer for the page
 function timer() {
     if (!running) {
         intervalID = setInterval(decrement, 1000)
         running = true;
     }
 }
-
+//This is how the timer counts down and calls an alert to let you know if you timed out
 function decrement() {
     count--
     $("#counter").text(count);
     if (count == 0) {
+        clearInterval(intervalID);
         running = false;
         timeUp()
-        clearInterval(intervalID);
+        
     }
 }
 
-
+//times up alert
 function timeUp() {
     alert("Times up!")
 }
 
+
+
+//jquery section but not entirely sure how this works all together.
+
 $("#start-button").click(timer);
 $("#quiz-answer-area").click(timer)
-$("#quiz-answer-area").append(myQuestions[0].question);
-$("#quiz-area").append(myQuestions[0].answers)
+// $("#quiz-question-area").append(myQuestions[0].question);
+$("#quiz-answer-area").append(myQuestions[0].answers)
+$("#quiz-question-area").text(myQuestions[next].question)
+   
