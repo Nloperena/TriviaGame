5b8c93ba650ce6ce5002f4da2454b600bb6b7d16
Nicholas Loperena <nicholasloperena@gmail.com>
2019-06-07 22:36:14 -0400

    added some more logic to my page and am messing around with more Jquery I moved it to the bottom of the app.js file, and am trying to figure out why my timer counts past 0 when I hit START GAME again. I think this will be resolved when I add another function that resets the timer and displays new questions

--- app.js
+++ app.js
@@ -1,61 +1,86 @@
 var running = false;
 
 // declaring variables
-var correct = [""];
-var incorrect = [""]
+var intervalID
+var j = 0
+var correct = 0
+var incorrect = 0
 // Step1
 var card = $("#quiz-area");
 
 // Step2
-var countStartNumber = 30;
+var count = 10;
 
 // Step3
-var question = [{
-        question: "string",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: "string",
-        incorrectAnswer: ["2", "3", "4"]
+
+var correctAnswers = ["1", "2", "3", "4", "5"];
+
+
+var myQuestions = [{
+        question: "This is my first question ",
+        answers: ["A1 ", "A2 ", "A3 ", "A4 "],
         // gif: //link to image
     },
     {
-        question: "string",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: "string",
-        incorrectAnswer: ["2", "3", "4"]
+        question: "This is my second question ",
+        answers: ["B1 ", "B2 ", "B3 ", "B4 "],
         // gif: //link to image
     },
     {
-        question: "string",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: "string",
-        incorrectAnswer: ["2", "3", "4"]
+        question: "This is my third question ",
+        answers: ["C1 ", "C2 ", "C3  ", "C4 "],
         // gif: //link to image;
     },
     {
-        question: "string",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: "string",
-        incorrectAnswer: ["2", "3", "4"]
+        question: "This is my fourth question ",
+        answers: ["D1 ", "D2 ", "D3 ", "D4 "],
         // gif: //link to image
     },
     {
-        question: "string",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: "string",
-        incorrectAnswer: ["2", "3", "4"]
+        question: "This is my fifth question ",
+        answers: ["E1 ", "E2 ", "E3 ", "E4 "],
         // gif: //link to image
     }
 ]
-//Step4
-function () {
+
+
+var game = {
+    //Step4
+    name: function () {
+        i = 0;
+        console.log(myQuestions[i].question)
+        for (i = 0; i < 4; i++) {
+            console.log(myQuestions[j].answers[i])
+        }
+        j++
+    }
+}
+game.name()
+
+// Step5
+function timer() {
     if (!running) {
-        var timer = setInterval(decrement, 1000)
+        intervalID = setInterval(decrement, 1000)
         running = true;
     }
 }
 
-
-var game = {
-
+function decrement() {
+    count--
+    $("#counter").text(count);
+    if (count == 0) {
+        running = false;
+        timeUp()
+        clearInterval(intervalID);
+    }
 }
 
+
+function timeUp() {
+    alert("Times up!")
+}
+
+$("#start-button").click(timer);
+$("#quiz-answer-area").click(timer)
+$("#quiz-answer-area").append(myQuestions[0].question);
+$("#quiz-area").append(myQuestions[0].answers)
